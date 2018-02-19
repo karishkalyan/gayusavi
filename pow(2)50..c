5d7b3d74ac5b86d6c274189de6ccc47ae2be5fe2
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-19 16:02:55 +0530

    power
    prgm

---
+++ pow(2)50..c
@@ -0,0 +1,12 @@
+3include<stdio.h>
+void main()
+{
+int n;
+printf("\n enter the number:");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n %d is a power of 2",n);
+else
+printf("\n it is not apower of 2");
+getch();
+}
